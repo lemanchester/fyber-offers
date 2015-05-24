f149c4bdcfc7f78e36cced5c301778fdb0e06a53
Lennon Manchester <le.manchester@gmail.com>
2015-05-24 14:58:21 -0300

    Add error model to wrapper errors from the API

---
+++ lib/fyber/error.rb
@@ -0,0 +1,16 @@
+module Fyber
+  class Error < StandardError
+
+    attr_reader :api_code, :message
+
+    def initialize(api_code, message)
+      @api_code = api_code
+      @message = message
+    end
+
+    def to_s
+      "#{api_code}: #{message}"
+    end
+
+  end
+end
