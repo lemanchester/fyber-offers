98c9203c0406128fdc3d149b0b39c955d89029f2
Lennon Manchester <le.manchester@gmail.com>
2015-05-24 20:26:12 -0300

    Add hability to Response itself when its a valid response from the API

--- lib/fyber/response.rb
+++ lib/fyber/response.rb
@@ -14,10 +14,15 @@ module Fyber
       @headers     = headers
     end
 
+    # @return [Response] if the response is valid return itself
+    # @raise [Fyber::Error] when status code != 200
+    # @raise [Fyber::InvalidResponseSignatura] when the header resp-sig don't match
+    # @raise [Fyber::NoContentError] when there is no content on the body
     def validate!
       raise_error(Error) if code != 200
       raise InvalidResponseSignature.new unless valid?
       raise_error(NoContentError) if parsed_body["code"] == "NO_CONTENT"
+      self
     end
 
     protected
--- spec/lib/fyber/response_spec.rb
+++ spec/lib/fyber/response_spec.rb
@@ -56,6 +56,29 @@ module Fyber
         end
       end
 
+      context "given a successful response with content" do
+        let(:code) { 200 }
+        let(:raw_body) do
+          File.read("spec/support/fixtures/offers_response.json")
+        end
+        let(:parsed_body) do
+          Yajl::Parser.parse(raw_body)
+        end
+
+        before do
+          allow(Encrypt).to receive(:generate).and_return("123")
+        end
+
+        it "returns itself" do
+          expect(subject.validate!).to be_a(Fyber::Response)
+        end
+
+        it "returns itself with parsed body" do
+          expect(subject.validate!.parsed_body).to eql(parsed_body)
+        end
+
+      end
+
     end
 
   end
