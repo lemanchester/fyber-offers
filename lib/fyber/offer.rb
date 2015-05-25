e49128b9ff0290c6d9cc963dc62a563a03bdf420
Lennon Manchester <le.manchester@gmail.com>
2015-05-24 21:33:26 -0300

    Add offers some code doc

--- lib/fyber/offer.rb
+++ lib/fyber/offer.rb
@@ -3,6 +3,8 @@ require "hashie"
 module Fyber
   class Offer < ::Hashie::Mash
 
+    # @param [Hash<Symbol,Strin>] list of params
+    # @return [Array<Fyber::Offer>] list of offers
     def self.all(params)
       resp = Request.new(:get, "offers", params).perform!
       resp.parsed_body['offers'].map do |element|
