222d081e55f97c6b767e8c2c31fbb3d0de23b6c0
Lennon Manchester <le.manchester@gmail.com>
2015-05-25 12:10:17 -0300

    Fix offer model to call request with the right arguments

--- lib/fyber/offer.rb
+++ lib/fyber/offer.rb
@@ -3,10 +3,10 @@ require "hashie"
 module Fyber
   class Offer < ::Hashie::Mash
 
-    # @param [Hash<Symbol,Strin>] list of params
+    # @param [Hash<Symbol,String>] list of params
     # @return [Array<Fyber::Offer>] list of offers
     def self.all(params)
-      resp = Request.new(:get, "offers", params).perform!
+      resp = Request.new(:get, "offers", params.delete(:api_key), params).perform!
       resp.parsed_body['offers'].map do |element|
         self.new(element)
       end
--- spec/lib/fyber/offer_spec.rb
+++ spec/lib/fyber/offer_spec.rb
@@ -13,17 +13,31 @@ module Fyber
       let(:response) do
         Response.new(200, raw_body, parsed_body, {})
       end
+      let(:params) do
+        {
+          uid: "157",
+          pub0: "player1",
+          page: 1
+        }
+      end
 
-      subject { described_class.all(params) }
+      subject { described_class }
+
+      context "given the list of params" do
+        let(:request_double) do
+          double("request", perform!: response)
+        end
+
+        it "call the request with the right params" do
+          expect(Request).to receive(:new).with(:get, "offers", "9999", params).and_return(request_double)
+          subject.all(params.merge({ api_key: "9999" }))
+        end
+      end
 
       context "given it return a list of offers" do
-        let(:params) do
-          {
-            uid: "157",
-            pub0: "player1",
-            page: 1
-          }
-        end
+
+        subject { described_class.all(params) }
+
         before do
           allow_any_instance_of(Request).to receive(:perform!).and_return(response)
         end
